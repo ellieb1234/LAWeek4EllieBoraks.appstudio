178b641f96c147ff3bc58b4345f9c2a3095c79c8
ellieb1234 <ellieboraks@creighton.edu>
2021-02-24 16:18:37 -0600

    Create pets.js

---
+++ LAWeek4Ellie BoraksBoraks.appstudio.appstudio/forms/pets/pets.js
@@ -0,0 +1,11 @@
+let petNames=["Darcy","Jack","Jerry","Marty","Vito","Vinny","Riley","Saddie","Marge","Sam"]
+//creates a symbolic link
+let withSal=petNames
+
+let name="Sal"
+withSal.push(name)
+
+console.log(`The third pet is named ${petNames[2]}.`)
+console.log(`The array has ${petNames.length} names in it.`)
+
+petNames.pop()
